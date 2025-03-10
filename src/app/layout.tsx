c755e408f5da8e9083cad5240aee2c76044c02df
ghirahm <ghirahmadanis@gmail.com>
2025-03-10 17:07:09 +0700

    fix: deployment error bugs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import Logo from './logo.ico';
-
 const geistSans = Geist({
     variable: "--font-geist-sans",
     subsets: ["latin"],
